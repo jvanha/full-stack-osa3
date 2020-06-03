87d85ee3db15fcb5f6981acd9bda0b21201fb5c5
Joonas Vanhalakka <joonas.vanhalakka@helsinki.fi>
2020-06-03 15:35:42 +0300

    uibuild

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
 
-app.use(express.static('buid'))
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 //app.use(morgan('tiny'))
