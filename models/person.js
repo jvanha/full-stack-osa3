1ea3343ea0da70210b6558b76aaf9361197e43eb
Joonas Vanhalakka <joonas.vanhalakka@helsinki.fi>
2020-06-05 13:13:02 +0300

    uibuild

--- models/person.js
+++ models/person.js
@@ -6,7 +6,7 @@ mongoose.set('useFindAndModify', false)
 const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
   .then(result => {
     console.log('connected to MongoDB')
   })
